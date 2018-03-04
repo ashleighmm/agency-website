38cf92179b316f2d3fd1903feed6c9f371bc4c13
ashleighm <ashleigh@hoist.co.za>
2018-03-04 14:43:53 +0000

    Files added

---
+++ africa.php
@@ -0,0 +1,60 @@
+<section id="africa">
+  <div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="false"> 
+    
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      
+      <div class="item active"> 
+        <!-- Begin stage wrapper -->
+        <div class="video-wrapper" style="background: url(images/jhb_skyline_1920x1080.jpg) no-repeat center center fixed !important; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;min-height:800px;"> 
+          
+          <!-- Background wrapper: -->
+          <div class="bg-wrapper" style="min-height:800px;">
+          
+            
+            <div class="block-wrapper">
+              <div class="content-wrapper"> 
+                <!-- Begin content block -->
+                <div class="content-block">
+                  <h1 class="white-shadow-text">What we do in <span class="red">South Africa </span></h1>
+                  <h3 class="white-shadow-text">We ignite new brands and rekindle troubled ones.</h3>
+                </div>
+                <!-- End content block --> 
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="item "> 
+        <!-- Begin stage wrapper -->
+        <div class="video-wrapper" style="background: url(images/night_market_1920x1080.jpg) no-repeat center center fixed !important; 
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;min-height:800px;"> 
+          
+          <!-- Background wrapper: -->
+          <div class="bg-wrapper" style="min-height:800px;">
+           
+            <div class="block-wrapper">
+              <div class="content-wrapper"> 
+                <!-- Begin content block -->
+                <div class="content-block">
+                  <h1 class="white-shadow-text">What we do in <span class="red">Africa</span></h1>
+                  <h3 class="white-shadow-text">We're fueling the wildfire growth of South African brands north of the Limpopo</h3>
+                </div>
+                <!-- End content block --> 
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <!-- Left and right controls --> 
+    <a class="left carousel-control back" href="#myCarousel" role="button" data-slide="prev"> <img src="images/left-arrow.png" width="30" height="36" style="display:block;" border="0" class="arrow-left"/><span class="sr-only">Previous</span> </a> <a class="right carousel-control forw" href="#myCarousel" role="button" data-slide="next"><img src="images/right-arrow.png" width="30" height="36" style="display:block;" border="0" class="arrow-left"/> <span class="sr-only">Next</span> </a> </div>
+</section>
